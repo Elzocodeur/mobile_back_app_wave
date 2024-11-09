dd0dd7520078f773a6bfbf6c0cd5f059281873a8
Elzocodeur <gningeli03@gmail.com>
2024-11-09 17:38:50 +0000

    release host

--- config/database.php
+++ config/database.php
@@ -83,20 +83,40 @@ return [
             ]) : [],
         ],
 
+        // local
+    
+        // 'pgsql' => [
+        //     'driver' => 'pgsql',
+        //     'url' => env('DB_URL'),
+        //     'host' => env('DB_HOST', '127.0.0.1'),
+        //     'port' => env('DB_PORT', '5432'),
+        //     'database' => env('DB_DATABASE', 'laravel'),
+        //     'username' => env('DB_USERNAME', 'root'),
+        //     'password' => env('DB_PASSWORD', ''),
+        //     'charset' => env('DB_CHARSET', 'utf8'),
+        //     'prefix' => '',
+        //     'prefix_indexes' => true,
+        //     'search_path' => 'public',
+        //     'sslmode' => 'prefer',
+        // ],
+
+
+        // sur render
         'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'prefer',
-        ],
+                'driver' => 'pgsql',
+                'url' => env('DB_URL'),
+                'host' => env('DB_HOST', 'ep-nameless-field-a5a7lpik.us-east-2.aws.neon.tech'), // Utilisez l'URL de votre instance Neon
+                'port' => env('DB_PORT', '5432'),
+                'database' => env('DB_DATABASE', 'neondb'),
+                'username' => env('DB_USERNAME', 'neondb_owner'),
+                'password' => env('DB_PASSWORD', 'ELYtPiVAQ50J'),
+                'charset' => env('DB_CHARSET', 'utf8'),
+                'prefix' => '',
+                'prefix_indexes' => true,
+                'search_path' => 'public',
+                'sslmode' => 'require', // Assurez-vous d'utiliser SSL
+            ],
+
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
